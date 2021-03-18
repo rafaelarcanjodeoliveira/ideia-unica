561936ce9734a29ce059da517a4c1a7f9be6a478
Rafael Arcanjo de Oliveira <rafael9559@hotmail.com>
2021-03-17 21:06:25 -0300

    primeira página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Página Principal</h1>
+    return (
+    <div>
+        
+        <h1>Página Principal</h1>
+
+        <h2>Rafael Arcanjo de Oliveira</h2>
+
+    </div>
+
+    ) 
 }
 
 export default Home
